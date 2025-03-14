4acee96dfe15d06f789e5f19e6d644a154cb00fe
Matthew Richards <matthew.richards884@gmail.com>
2025-03-14 13:49:45 +0000

    Attempt to fix tailwind not working

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,18 @@
 import { type Config } from 'tailwindcss';
 
 export default {
-  content: ['./app/**/*.{html,js,jsx,ts,tsx}'],
+  content: [
+    './app/**/*.{html,js,jsx,ts,tsx}',
+    './App/**/*.{html,js,jsx,ts,tsx}',
+    './Components/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+    './_build/**/*.{js,ts,jsx,tsx}',
+    './_Build/**/*.{js,ts,jsx,tsx}',
+    './_server/**/*.{js,ts,jsx,tsx}',
+    './_Server/**/*.{js,ts,jsx,tsx}',
+    './assets/**/*.{js,ts,jsx,tsx}',
+    './Assets/**/*.{js,ts,jsx,tsx}',
+  ],
   theme: {
     extend: {
       fontFamily: {
